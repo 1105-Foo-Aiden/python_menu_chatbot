b522a93e82d23f81204a4b360a943e60e9b6e90a
Aiden Foo <aidenfoo154@gmail.com>
2024-09-22 15:58:01 -0700

    more options + adding a delay for order

--- chatbot.py
+++ chatbot.py
@@ -1,3 +1,5 @@
+import time
+
 def Message():
     print("Invalid choice, please try again")
 
@@ -5,11 +7,25 @@ def get_name():
     name = input("Enter your name: ")
     return name
 
+def Milk_choice():
+    print("What kind of milk would you like?")
+    type = input("[a] Whole milk \n[b] Almond milk \n[c] Oat milk \n")
+    if type == 'a':
+        return "Whole milk"
+    elif type == 'b':
+        return "Almond Milk"
+    elif type == 'c':
+        return "Oat Milk"
+    else:
+        Message()
+        Milk_choice()
+
 def latte():
     print("You ordered a Latte. Would you like it with milk or without milk?")
     milk = input("[a] With milk\n[b] Without milk \n")
     if milk == 'a':
-        return "Latte with milk"
+        milk_choice = Milk_choice()
+        return (f"Latte with {milk_choice}")
     elif milk == 'b':
         return "Latte without milk"
     else:
@@ -20,7 +36,8 @@ def Coffee():
     print("You ordered a Coffee. Would you like it with sugar or without sugar?")
     req = input("[a] Yes I would like Sugar\n[b] No I would not like sugar\n")
     if req == 'a':
-        return "Coffee with sugar"
+        sugars = input("How many sugars would you like? \n")
+        return (f"Coffee with {sugars} sugar(s)")
     elif req == 'b':
         return "Coffee without sugar"
     else :
@@ -28,7 +45,7 @@ def Coffee():
         Coffee()
         
 def Frap():
-    print("You ordered a a Frappuccino. Would you like it with whipped cream or without?\n")
+    print("You ordered a a Frappuccino. Would you like it with or without whipped cream?\n")
     choice = input("[a] With\n[b] Without\n")
     if choice == 'a':
         return "Frap with whipped cream"
@@ -43,6 +60,8 @@ def confrm_Order(drink):
     if correct == 'y':
         name = get_name()
         print(f"Thank you {name}, your order will be out shortly")
+        time.sleep(2)
+        print(f"Here you are {name}, your {drink}. Thank you for shopping with us, please come again")
     elif correct == 'n':
         print("I'm sorry to hear that, let's start over")
         get_order()
